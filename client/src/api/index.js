ea2828fdbc1b12712e6fdc7f595026505783b8d4
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-04-04 14:34:50 +0100

    done

--- client/src/api/index.js
+++
@@ -1,4 +0,0 @@
-export { default as registerFn } from "./register";
-export { default as loginFn } from "./login";
-export { default as logoutFn } from "./logout";
-export { default as refreshTokenFn } from "./refreshToken";
